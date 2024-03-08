f94d740872657faeb5c785ae66f780f88632a666
Duy Truong <truongduy.fu@gmail.com>
2024-03-09 03:05:07 +0700

    set-up and test babel-plugin-module-resolver

--- config-overrides.js
+++ config-overrides.js
@@ -1,6 +1,7 @@
-/* config-overrides.js */
+const { override, useBabelRc } = require('customize-cra');
 
-module.exports = function override(config, env) {
-  //do stuff with the webpack config...
-  return config;
-};
+// config-overrides.js
+module.exports = override(
+  // eslint-disable-next-line react-hooks/rules-of-hooks
+  useBabelRc()
+);
